dc58e6a1aacaa0be40e73d4c3f10228ccb757091
Mohamed Boussas <mohamedboussas0@gmail.com>
2025-07-07 05:00:00 +0100

    implemented bubble sort in java

---
+++ Java/bubble_sort.java
@@ -0,0 +1,35 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.print("Original: ");
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+
+        bubbleSort(arr);
+
+        System.out.print("Sorted: ");
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+    }
+}
+
