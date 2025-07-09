6cfb402c17bc2af656e43f4034e349765f506bb6
Mohamed Boussas <mohamedboussas0@gmail.com>
2025-07-09 05:00:00 +0100

    implemented bubble sort in javascript

---
+++ JavaScript/bubble_sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n; i++) {
+        let swapped = false;
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+    return arr;
+}
+
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original:", arr);
+let sortedArr = bubbleSort(arr);
+console.log("Sorted:", sortedArr);
